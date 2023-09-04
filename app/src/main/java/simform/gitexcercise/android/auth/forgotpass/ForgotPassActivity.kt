c23b700e453297ffce85159aaa70d08e4239206f
parasharandroid <parashar@simformslutions.com>
2023-09-04 15:51:04 +0530

    auth module completed Auth screens completed [Done]
    created login screen [Done]
    
    created registration screen [Done]
    
    created forgot pass screen [Done]

---
+++ app/src/main/java/simform/gitexcercise/android/auth/forgotpass/ForgotPassActivity.kt
@@ -0,0 +1,12 @@
+package simform.gitexcercise.android.auth.forgotpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import simform.gitexcercise.android.R
+
+class ForgotPassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_pass)
+    }
+}
