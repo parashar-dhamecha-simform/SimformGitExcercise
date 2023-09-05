64343a1795e8062e0f0280916a2d647da1d013f6
parasharandroid <parashar@simformslutions.com>
2023-09-05 13:21:37 +0530

    created profile screen [Done] **

---
+++ app/src/main/java/simform/gitexcercise/android/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package simform.gitexcercise.android.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import simform.gitexcercise.android.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
